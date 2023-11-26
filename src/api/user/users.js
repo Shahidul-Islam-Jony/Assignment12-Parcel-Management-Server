dd54a4845a4ab02cb1f8938337b3cdc88e0fab88
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-26 22:44:00 +0600

    User info saved in mongoDB database using mongoose

--- src/api/user/users.js
+++ src/api/user/users.js
@@ -3,10 +3,13 @@ const User = require("../../models/User");
 
 const users = async (req, res) => {
     const userInfo = req.body;
-    console.log('hello');
-    const result = new User(userInfo)
-    await result.save();
-    res.send(result)
+    const query = { email: userInfo.email }
+    const isUserExists = await User.findOne(query)
+    if (!isUserExists) {
+        const result = new User(userInfo)
+        await result.save();
+        res.send(result)
+    }
 }
 
 module.exports = users
